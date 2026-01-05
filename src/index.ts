a5c6428aa7903b854663d3453f315d821f1fe5e3
cer-sk385 <shin.ne1365@gmail.com>
2026-01-05 22:47:00 +0900

    Add /test.html endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import testHtml from './test.html';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
@@ -19,6 +8,10 @@ export default {
 				return new Response('Hello, World!');
 			case '/random':
 				return new Response(crypto.randomUUID());
+			case '/test.html':
+				return new Response(testHtml, {
+					headers: { 'Content-Type': 'text/html; charset=utf-8' },
+				});
 			default:
 				return new Response('Not Found', { status: 404 });
 		}
